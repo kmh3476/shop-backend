1b7df7d211182a54f7c9d7ea50236671ebf3199d
BuildTools <unconfigured@null.spigotmc.org>
2025-11-11 22:48:57 +0900

    Add Tailwind setup and configuration

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const allowedOrigins = [
   "https://www.onyou.store",
   "https://project-onyou.vercel.app",
   "https://shop-frontend-cz3y-kmh3476s-projects.vercel.app",
-  "https://shop-frontend-cz3y-qbv3xe3mc-kmh3476s-projects.vercel.app",
+  "https://shop-frontend-cz3y-mkjztpjni-kmh3476s-projects.vercel.app",
   "https://shop-backend-1-dfsl.onrender.com",
 ];
 
